c9bf92d09eaf58f0e159becfdc9585541937bc1f
Stefano Lande <landeste@gmail.com>
2018-07-23 17:12:21 +0200

    handled different keys for terms; added placeholder signatures

--- bitml-test.rkt
+++ bitml-test.rkt
@@ -3,6 +3,9 @@
 (participant "A" "029c5f6f5ef0095f547799cb7861488b9f4282140d59a6289fbc90c70209c1cced")
 (participant "B" "029c5f6f5ef0095f547799cb7861488b9f4282140d59a6289fbc90c70209c1cced")
 
+(key "A" (auth "A" (after 30 (after 20 (withdraw "B")))) "029c5f6f5ef0095f547799cb7861488b9f4282140d59a6289fbc90c70209c1cced")
+(key "B" (auth "A" (after 30 (after 20 (withdraw "B")))) "029c5f6f5ef0095f547799cb7861488b9f4282140d59a6289fbc90c70209c1cced")
+
 (advertise (guards (deposit "A" 1 "txA@0")
                    (deposit "A" 1 "txA1@0")
                    (deposit "B" 2 "txB@0"))
--- main.rkt
+++ main.rkt
@@ -1,14 +1,14 @@
 #lang racket/base
 
 (require (for-syntax racket/base syntax/parse)
-          racket/list)
+         racket/list racket/bool)
 
 ;provides the default reader for an s-exp lang
 (module reader syntax/module-reader
   bitml)
 
 (provide (all-from-out racket/base)
-         participant advertise withdraw deposit guards after auth)
+         participant advertise withdraw deposit guards after auth key)
 
 ;function to enumerate tx indexes
 (define tx-index 0)
@@ -17,7 +17,10 @@
   (set! tx-index (add1 tx-index))
   tx-index)
 
-;helpers to store and retrieve participants info
+(define (new-tx-name)
+  (format "T~a" (new-tx-index)))
+
+;helpers to store and retrieve participants' public keys
 (define participants-table
   (make-hash))
 
@@ -30,54 +33,78 @@
 (define (get-participants)
   (hash-keys participants-table))
 
+;helpers to store and retrieve participants' public keys for terms
+(define pk-terms-table
+  (make-hash))
+
+(define (add-pk-for-term id term pk)
+  (hash-set! pk-terms-table (cons id term) pk))
+
+(define (pk-for-term id term)
+  (hash-ref pk-terms-table (cons id term)))
+
 ;helpers to generate string transactions
 (define (slist->string l)
   (foldr (lambda (s r) (string-append s r)) "" l))
-
-(define (participants->tx-params participants)
-  (define s
-    (foldl (lambda (p acc) (string-append  "s" (format "~a" p) ":signature," acc))  "" participants))
-  (substring s 0 (sub1 (string-length s))))
 
 (define (participants->tx-params-list participants)
   (for/list([p participants])
     (string-append  "s" (format "~a" p))))
 
-(define (participants->tx-sigs participants)
-  (foldl (lambda (p acc) (string-append  "s" (format "~a" p) " " acc))  "" participants))
+(define (participants->tx-sigs participants tx-name)
+  (foldl (lambda (p acc) (format "sig~a~a ~a" p tx-name acc))  "" participants))
+
+(define (participants->tx-sigsl participants tx-name)
+  (map (lambda (p) (format "sig~a~a ~a" p tx-name)) participants))
+
+(define (participants->sigs-declar participants tx-name [contract #f])
+  (foldr (lambda (p acc) (format "const sig~a~a : signature = _ ~a\n~a" p tx-name
+                                 (if (false? contract)
+                                     ""
+                                     (string-append "//signature with private key of " (pk-for-term p contract)))
+                                 acc))
+         "" participants))
+
 
 ;declaration of a participant
 ;associates a name to a public key
-#;
-(define-simple-macro (participant ident:string pubkey:string)
-  (add-participant 'ident pubkey))
-
 (define-syntax (participant stx)
   (syntax-parse stx
     [(_ ident:string pubkey:string)
      #'(add-participant 'ident pubkey)]))
 
+;declaration of a participant
+;associates a name and a term to a public key
+(define-syntax (key stx)
+  (syntax-parse stx
+    [(_ ident:string term pubkey:string)
+     #'(add-pk-for-term 'ident 'term pubkey)]))
+
 ;compiles withdraw to transaction
 (define-syntax (withdraw stx)
   (syntax-parse stx    
-    [(_ part parent-tx input-idx value parts timelock)
-     #'(begin
-         (define tx-params (participants->tx-params parts))
-         (define tx-sigs (participants->tx-sigs parts))
+    [(_ part parent-contract parent-tx input-idx value parts timelock)
+     #'(begin         
+         (define tx-name (new-tx-name))
+         (define tx-sigs (participants->tx-sigs parts tx-name))
 
-         (displayln (if (> timelock 0)
-                        (format "transaction T~a(~a,~a) { \n input = ~a@~a:~a \n output = ~a BTC : fun(x) . versig(addr~a; x) \n absLock = block ~a \n}\n"
-                                (new-tx-index) tx-params parent-tx parent-tx input-idx tx-sigs value part timelock)
-                        (format "transaction T~a(~a,~a) { \n input = ~a@~a:~a \n output = ~a BTC : fun(x) . versig(addr~a; x) \n}\n"
-                                (new-tx-index) tx-params parent-tx parent-tx input-idx tx-sigs value part))))]
+
+         (displayln (participants->sigs-declar parts tx-name parent-contract))
+         
+         (displayln (string-append
+                     (format "transaction ~a { \n input = ~a@~a:~a \n output = ~a BTC : fun(x) . versig(addr~a; x) \n "
+                             tx-name parent-tx input-idx tx-sigs value part)
+                     (if (> timelock 0)
+                         (format "absLock = block ~a \n}\n" timelock)
+                         "\n}\n"))))]
     [(_)
      (raise-syntax-error stx '! "cannot use withdraw alone")]))
 
 ;handles after
 (define-syntax (after stx)
   (syntax-parse stx   
-    [(_ t (contract params ...) parent-tx input-idx value parts timelock)
-     #'(contract params ... parent-tx input-idx value parts (max t timelock))]
+    [(_ t (contract params ...) parent-contract parent-tx input-idx value parts timelock)
+     #'(contract params ... parent-contract parent-tx input-idx value parts (max t timelock))]
     
     [(_)
      (raise-syntax-error stx '! "cannot use after alone")]))
@@ -85,15 +112,19 @@
 ;handles auth
 (define-syntax (auth stx)
   (syntax-parse stx   
-    [(_ part:string (contract params ...) parent-tx input-idx value parts timelock)
-     #'(contract params ... parent-tx input-idx value (remove part parts) timelock)] 
+    [(_ part:string (contract params ...) orig-contract parent-tx input-idx value parts timelock)
+     ;#'(contract params ... parent-tx input-idx value (remove part parts) timelock)]
+     #'(contract params ... orig-contract parent-tx input-idx value parts timelock)] 
+
     [(_)
      (raise-syntax-error stx '! "cannot use auth alone")]))
      
 ;keywords for the next macro
 (define-syntax (guards stx) (raise-syntax-error 'guards "cannot use guards alone" stx))
 (define-syntax (deposit stx) (raise-syntax-error 'deposit "cannot use deposit alone" stx))
+
 ;compilation command
+;todo: output script
 (define-syntax (advertise stx)
   (syntax-parse stx
     #:literals (guards deposit)    
@@ -104,22 +135,28 @@
      #`(begin
          (define deposit-parts (list 'part ...))
          (define deposit-txout (list txout ...))
-         (define tx-params-list (for/list ([p deposit-parts]
+         (define tx-sigs-list (for/list ([p deposit-parts]
                                            [i (in-naturals)])
-                                  (format "~a~a" p i)))     
-         (define tx-params-string
+                                  (format "sig~a~a" p i)))     
+         #;(define tx-params-string
            (string-append (first tx-params-list)
                           (slist->string (for/list ([p (rest tx-params-list)])                                      
                                            (format ",~a" p)))))       
          (define tx-v (+ v ...))
          
-         ;dichiara le pk
-         (for-each (lambda (s) (displayln (format "const pubkey~a = pubkey:~a" s (participant-pk s)))) (get-participants))         
-         (define inputs (string-append "input = [ "
-                                       (format "~a:~a" (first deposit-txout) (first tx-params-list))
-                                       (slist->string (for/list ([p (rest tx-params-list)] [out (rest deposit-txout)])
-                                                        (format "; ~a:~a" out p))) " ]"))                   
-         (displayln (format "\ntransaction Tinit(~a) { \n ~a \n output = ~a BTC \n}\n" tx-params-string inputs tx-v))
+         ;compile public key
+         (for-each (lambda (s) (displayln (format "const pubkey~a = pubkey:~a" s (participant-pk s)))) (get-participants))
+         (displayln "")
 
-         ;procedi compilando il contratto
-         (contract params ... "Tinit" 0 tx-v (get-participants) 0))]))
+         ;compile signatures constants for Tinit
+         (for-each (lambda (e) (displayln (string-append "const " e " : signature = _"))) tx-sigs-list)
+
+         
+         (define inputs (string-append "input = [ "
+                                       (format "~a:~a" (first deposit-txout) (first tx-sigs-list))
+                                       (slist->string (for/list ([p (rest tx-sigs-list)] [out (rest deposit-txout)])
+                                                        (format "; ~a:~a" out p))) " ]"))                   
+         (displayln (format "\ntransaction Tinit { \n ~a \n output = ~a BTC \n}\n" inputs tx-v))
+
+         ;start the compilation of the contract
+         (contract params ... '(contract params ...) "Tinit" 0 tx-v (get-participants) 0))]))
