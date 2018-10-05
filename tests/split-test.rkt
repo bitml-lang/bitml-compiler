8ecb76a25d1676e338097eab1beea5a6be426070
Stefano Lande <landeste@gmail.com>
2018-10-05 12:41:27 +0200

    files reorg

--- tests/split-test.rkt
+++ tests/split-test.rkt
@@ -1,4 +1,4 @@
-#lang s-exp "../main.rkt"
+#lang bitml
 
 (participant "A" "029c5f6f5ef0095f547799cb7861488b9f4282140d59a6289fbc90c70209c1cced")
 (participant "B" "022c3afb0b654d3c2b0e2ffdcf941eaf9b6c2f6fcf14672f86f7647fa7b817af30")
@@ -12,6 +12,6 @@
 
 
 
-(compile (guards (deposit "A" 4 "txA@0")(secret a "000a")(deposit "B" 0 "txB@0"))        
+(compile (guards (deposit "A" 1 "txA@0")(secret a "000a")(deposit "B" 0 "txB@0"))        
          (after 10 (split (3 (sum (withdraw "A") (withdraw "B"))) (1 (sum (withdraw "B"))))))
 
