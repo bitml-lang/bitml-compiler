344ec4b0b7fdbfd46f8b1fafaaf7248c06260c03
bart <bart@unica.it>
2019-05-13 14:55:29 +0200

    added escrow.rkt

---
+++ examples/escrow.rkt
@@ -0,0 +1,39 @@
+#lang bitml
+
+(participant "A" "029c5f6f5ef0095f547799cb7861488b9f4282140d59a6289fbc90c70209c1cced")
+(participant "B" "022c3afb0b654d3c2b0e2ffdcf941eaf9b6c2f6fcf14672f86f7647fa7b817af30")
+(participant "O" "029e72227cb7a26eb45813c4a4ecb50894564bc9fe04ff4060f3b05f1ec5587738")
+
+(debug-mode)
+(verification-only)
+
+(define (PayOrRefund)
+  (choice
+   (auth "A" (withdraw "B"))
+   (auth "B" (withdraw "A"))
+   )
+  )
+
+(define (Resolve v w)
+  (split
+   (v -> (withdraw "O"))
+   (w -> (choice (auth "O" (withdraw "A")) (auth "O" (withdraw "B"))))
+  )
+  )
+
+(contract
+ (pre (deposit "A" 1 "txA@0"))
+ (choice
+  (ref (PayOrRefund))
+  (auth "A" (ref (Resolve 0.1 0.9)))
+  (auth "B" (ref (Resolve 0.1 0.9)))
+  ; (after 10000 (withdraw "A"))
+  )          
+
+ (check-liquid (strategy "A" (do-auth (auth "A" (ref (Resolve 0.1 0.9)))))) ; false
+               
+ (check-liquid (strategy "O" (do-auth (auth "O" (withdraw "A")))))          ; false
+ 
+ (check-liquid (strategy "A" (do-auth (auth "A" (ref (Resolve 0.1 0.9)))))
+               (strategy "O" (do-auth (auth "O" (withdraw "A")))))          ; true
+ )
