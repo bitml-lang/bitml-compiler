d844c5c30186941ec734f90df51b14840c24bd7c
bart <bart@unica.it>
2019-05-14 16:07:31 +0200

    revised 4p-lottery

--- examples/gambling/4p-lottery.rkt
+++ examples/gambling/4p-lottery.rkt
@@ -22,39 +22,34 @@
              (choice
               (revealif (c1 d1) (pred (= c1 d1)) (ref (Round2 "A" a2 "C" c2)))
               (revealif (c1 d1) (pred (!= c1 b1)) (ref (Round2 "A" a2 "D" d2)))
-              (after 10 (split (1 -> (withdraw "A")) (1 -> (withdraw "B")) (1 -> (withdraw "C")) (1 -> (withdraw "D"))))))
+              (after 20 (split (1 -> (withdraw "A")) (1 -> (withdraw "B")) (1 -> (withdraw "C")) (1 -> (withdraw "D"))))))
    (revealif (a1 b1) (pred (!= a1 b1))
              (choice
               (revealif (c1 d1) (pred (= c1 d1)) (ref (Round2 "B" b2 "C" c2)))
               (revealif (c1 d1) (pred (!= c1 b1)) (ref (Round2 "B" b2 "D" d2)))
-              (after 10 (split (1 -> (withdraw "A")) (1 -> (withdraw "B")) (1 -> (withdraw "C")) (1 -> (withdraw "D"))))))
+              (after 20 (split (1 -> (withdraw "A")) (1 -> (withdraw "B")) (1 -> (withdraw "C")) (1 -> (withdraw "D"))))))
    (after 10 (split (1 -> (withdraw "A")) (1 -> (withdraw "B")) (1 -> (withdraw "C")) (1 -> (withdraw "D"))))))
 
 (define (Round2 P1 x1 P2 x2)
   (choice
    (revealif (x1 x2) (pred (= x1 x2)) (withdraw P1))
    (revealif (x1 x2) (pred (!= x1 x2)) (withdraw P2))
-   (after 10 (split (1 -> (withdraw P1)) (1 -> (withdraw P2))))
+   (after 20 (split (2 -> (withdraw P1)) (2 -> (withdraw P2))))
    )
   )
 
 (contract (pre
-           (deposit "A" 7 "txA@0")(deposit "B" 7 "txB@0")(deposit "C" 7 "txC@0")(deposit "D" 7 "txD@0")
+           (deposit "A" 13 "txA@0")(deposit "B" 13 "txB@0")(deposit "C" 13 "txC@0")(deposit "D" 13 "txD@0")
            (secret "A" a1 "c51b66bced5e4491001bd702669770dccf440982") (secret "A" a2 "f9292914bfd27c426a23465fc122322abbdb63b7")
            (secret "B" b1 "9804ebb0fc4a8329981dd33aaff32b6cb579580a") (secret "B" b2 "18ed15665ab53ba8f4c965748e8a657cf40ee3f2")
            (secret "C" c1 "183c86e0a286ac99ad8cf5c4cde36511181ffbd5") (secret "C" c2 "ded836a730cdeca5223f2609747074585f933aa8")
            (secret "D" d1 "14f06dde2fa12bd359ea0847de296f7b66a0f93f") (secret "D" d2 "7249ab836ec75abf7756aec7528812a86a9f23df"))
          
           (split
-           (3 -> (ref (TC "A" a1 "B" "C" "D")))
-           (3 -> (ref (TC "A" a2 "B" "C" "D")))
-           (3 -> (ref (TC "B" b1 "A" "C" "D")))
-           (3 -> (ref (TC "B" b2 "A" "C" "D")))
-           (3 -> (ref (TC "C" c1 "A" "B" "D")))
-           (3 -> (ref (TC "C" c2 "A" "B" "D")))
-           (3 -> (ref (TC "D" d1 "A" "B" "C")))
-           (3 -> (ref (TC "D" d2 "A" "B" "C")))
-           
+           (12 -> (ref (TC "A" a1 "B" "C" "D")))
+           (12 -> (ref (TC "B" b1 "A" "C" "D")))
+           (12 -> (ref (TC "C" c1 "A" "B" "D")))
+           (12 -> (ref (TC "D" d1 "A" "B" "C")))
            (4 -> (ref (Round1))))
 
           (check-liquid
